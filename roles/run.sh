a6aa50aaac8ff06461131ccf8bf6a954f6e3253f
aditya <adityavittal92@gmail.com>
2024-11-10 21:18:41 +0530

    update
    update

---
+++ roles/run.sh
@@ -0,0 +1,1 @@
+ansible-playbook -i ${component}-${env_name}.doubtfree.online, -e ansible_user=ec2-user -e ansible_password=${SSH_PASSWORD} -e role_name=${component} -e env_name=${env_name} expense.yml
