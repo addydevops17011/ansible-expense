0291e92a8bc5dea367c8c85aecfb985d7d10b858
aditya <adityavittal92@gmail.com>
2024-11-10 21:46:37 +0530

    update
    updaet

--- run.sh
+++ run.sh
@@ -1,1 +1,5 @@
+if [ "$pipeline" == "true" ]; then
+exit
+fi
+
 ansible-playbook -i ${component}-${env_name}.doubtfree.online, -e ansible_user=ec2-user -e ansible_password=${SSH_PASSWORD} -e role_name=${component} -e env_name=${env_name} expense.yml
